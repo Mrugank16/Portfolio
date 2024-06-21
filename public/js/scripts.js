9f5d247aeefc18b281fb2fd4aca71325e4703f19
[mrugank raut] <[raut21bcs39@iiitkottayam.ac.in]>
2024-06-21 15:36:39 +0530

    Updated with progress bars

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,21 @@
-console.log("JavaScript is working!");
+document.addEventListener('DOMContentLoaded', function() {
+    const progressBars = document.querySelectorAll('.progress');
+  
+    progressBars.forEach(bar => {
+      bar.style.transition = 'width 2s';
+      bar.style.width = bar.getAttribute('data-width');
+    });
+  });
+
+  
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+  
+    if (name === '' || email === '' || message === '') {
+      event.preventDefault();
+      alert('All fields are required.');
+    }
+  });
+  
